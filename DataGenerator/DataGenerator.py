8bfec9cb49a34a2160602bec35dd11db234ed7ed
Kristian Skov <kris701kj@gmail.com>
2021-10-07 09:21:23 +0200

    * Fixed merge issues

--- DataGenerator/DataGenerator.py
+++ DataGenerator/DataGenerator.py
@@ -19,3 +19,24 @@ ftg.Generate()
 
 print("Dataset generated!")
 input("Press Enter to exit...")
+from CSVGenerator import CSVGenerator
+from FileLoader import FileLoader
+from FileTreeGenerator import FileTreeGenerator
+
+fl = FileLoader("./DataGenerator/InputText/", "./DataGenerator/InputLetters/by_class/", "https://s3.amazonaws.com/nist-srd/SD19/by_class.zip",
+                [['AChristmasCarol', 'https://www.gutenberg.org/files/46/46-0.txt'], ['PrideandPrejudice', 'https://www.gutenberg.org/files/1342/1342-0.txt']])
+fl.CheckAndCreatePaths()
+fl.LoadLetterPaths()
+fl.GatherLetterPaths()
+
+cg = CSVGenerator("./DataGenerator/data.csv", ('Letter', 'Path'))
+cg.GenerateCSVData(fl.TextFileStream, fl.LetterPaths,
+                   fl.TextPath, fl.TextFileQueue)
+
+fl.Finish()
+
+ftg = FileTreeGenerator("./DataGenerator/data.csv", "./DataGenerator/Data/")
+ftg.Generate()
+
+print("Dataset generated!")
+input("Press Enter to exit...")
