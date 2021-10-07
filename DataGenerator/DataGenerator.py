5f2d69309f13066cf5c830d24249bf89bad8d2ee
Kristian Skov <kris701kj@gmail.com>
2021-10-07 11:20:06 +0200

    * Config file test, to see how projects communicate together

--- DataGenerator/DataGenerator.py
+++ DataGenerator/DataGenerator.py
@@ -1,28 +1,22 @@
-import configparser
-import json
-import os
-config = configparser.ConfigParser()
-if os.path.exists('override-config.ini'):
-    config.read(['config.ini','override-config.ini'])
-else:
-    config.read('config.ini')
+import ConfigHelper as cfg
 
+import json
 from CSVGenerator import CSVGenerator
 from FileLoader import FileLoader
 from FileTreeGenerator import FileTreeGenerator
 
 fl = FileLoader(
-    config["DATAGENERATOR"]["TextPath"].strip('"'), 
-    config["DATAGENERATOR"]["LetterPath"].strip('"'), 
-    config["DATAGENERATOR"]["LetterDownloadURL"].strip('"'), 
-    json.loads(config["DATAGENERATOR"]["TextDownloadURLS"]),
-    config["DATAGENERATOR"]["TempDownloadLetterPath"].strip('"'))
+    cfg.config["DATAGENERATOR"]["TextPath"].strip('"'), 
+    cfg.config["DATAGENERATOR"]["LetterPath"].strip('"'), 
+    cfg.config["DATAGENERATOR"]["LetterDownloadURL"].strip('"'), 
+    json.loads(cfg.config["DATAGENERATOR"]["TextDownloadURLS"]),
+    cfg.config["DATAGENERATOR"]["TempDownloadLetterPath"].strip('"'))
 fl.CheckAndCreatePaths()
 fl.LoadLetterPaths()
 fl.GatherLetterPaths()
 
 cg = CSVGenerator(
-    config["DATAGENERATOR"]["CSVFileName"].strip('"'), 
+    cfg.config["DATAGENERATOR"]["CSVFileName"].strip('"'), 
     ('Letter', 'Path'))
 cg.GenerateCSVData(fl.TextFileStream, fl.LetterPaths,
                    fl.TextPath, fl.TextFileQueue)
@@ -30,8 +24,8 @@ cg.GenerateCSVData(fl.TextFileStream, fl.LetterPaths,
 fl.Finish()
 
 ftg = FileTreeGenerator(
-    config["DATAGENERATOR"]["CSVFileName"].strip('"'), 
-    config["DATAGENERATOR"]["OutputLettersPath"].strip('"'))
+    cfg.config["DATAGENERATOR"]["CSVFileName"].strip('"'), 
+    cfg.config["DATAGENERATOR"]["OutputLettersPath"].strip('"'))
 ftg.Generate()
 
 print("Dataset generated!")
---
+++ ProjectTools/ConfigHelper.py
@@ -0,0 +1,7 @@
+import configparser
+import os
+config = configparser.ConfigParser()
+if os.path.exists('override-config.ini'):
+    config.read(['config.ini','override-config.ini'])
+else:
+    config.read('config.ini')
