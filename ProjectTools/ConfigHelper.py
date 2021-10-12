1ed595751471ff8e4342e292677851fae3152594
Kristian Skov <kris701kj@gmail.com>
2021-10-12 09:31:14 +0200

    * Set all the projects as being packages instead of just files

--- ProjectTools/ConfigHelper.py
+++ ProjectTools/ConfigHelper.py
@@ -1,5 +1,7 @@
 import configparser
 import os
+import typing
+
 config = configparser.ConfigParser()
 if os.path.exists('override-config.ini'):
     config.read(['config.ini','override-config.ini'])
--- ProjectToolsTests/ConfigHelperTests.py
+++ ProjectToolsTests/ConfigHelperTests.py
@@ -3,10 +3,10 @@ import unittest
 class ConfigHelperTests(unittest.TestCase):
     def test_CanLoadConfig(self):
         # ARRANGE
-        import ProjectTools.ConfigHelper as cfg
-        
+        from ProjectTools import ConfigHelper as cfg
+
         # ACT
-        length = len(cfg.config['DATAGENERATOR'])
+        length : int = len(cfg.config['DATAGENERATOR'])
 
         # ASSERT
         self.assertLess(0, length)
