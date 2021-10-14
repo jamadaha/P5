59e5f793389bbe108cb6dd77ed441ab86f4719c7
Kristian Skov <kris701kj@gmail.com>
2021-10-14 11:02:07 +0200

    * Also renamed the environment variable

--- ProjectTools/AutoPackageInstaller.py
+++ ProjectTools/AutoPackageInstaller.py
@@ -2,20 +2,20 @@ import sys
 import subprocess
 import os
 
-os.environ["AutoModule_YesToAllModules"] = "False"
+os.environ["AutoPackageInstaller_YesToAllModules"] = "False"
 
 def CheckAndInstall(packageName):
     try:
         return __import__(packageName)
     except ImportError:
-        if os.environ["AutoModule_YesToAllModules"] == "True":
+        if os.environ["AutoPackageInstaller_YesToAllModules"] == "True":
             print(" --- Installing package " + packageName + " ---")
             subprocess.check_call([sys.executable, "-m", "pip", "install", packageName])
         else:
             while True:
                 Question = input("Package '" + packageName + "' is missing. Wanna install it? (y/n)(type Y to say yes to all):")
                 if Question == "Y":
-                    os.environ["AutoModule_YesToAllModules"] = "True";
+                    os.environ["AutoPackageInstaller_YesToAllModules"] = "True";
                     CheckAndInstall(packageName)
                     break
                 elif Question == "y":
