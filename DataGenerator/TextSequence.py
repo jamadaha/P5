2930c1748fae5e9018f7c4cbc12b55f36d192cdc
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-10-20 13:07:50 +0200

    Loads new textstream on running out of letters

--- DataGenerator/TextSequence.py
+++ DataGenerator/TextSequence.py
@@ -7,17 +7,26 @@ class TextSequence:
 
     def __init__(self, textPath):
         self.TextPath = textPath
-        self.LoadTextStream()
+        self.ImportTexts()
+        self.LoadNextStream()
 
-    def LoadTextStream(self):
+    def ImportTexts(self):
         import os
         print("Loading texts")
         self.TextFileQueue = os.listdir(self.TextPath)
         if not self.TextFileQueue:
             raise Exception("Text files not found!")
+        
+
+    def LoadNextStream(self):
         self.TextFileStream = open(
             self.TextPath + self.TextFileQueue[0], 'r', encoding='utf-8')
         self.TextFileQueue.pop(0)
 
     def GetNextLetter(self):
-        return self.TextFileStream.read(1)
+        char = self.TextFileStream.read(1)
+        if len(char) == 0:
+            if len(self.TextFileQueue) == 0:
+                raise Exception("Ran out of text files, please add more")
+            self.LoadNextStream()
+        return char
