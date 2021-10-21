a68a0f3313166231ad9b4c5d954c69c00c44f987
Kristian Skov <kris701kj@gmail.com>
2021-10-21 21:50:45 +0200

    * Put the methods for generating letters from the trained generator in a seperate class file

--- GAN/CGAN.py
+++ GAN/CGAN.py
@@ -1,10 +1,13 @@
 import sys
 sys.path.append('./ProjectTools')
 
+import ConfigHelper as cfg
+
 import DatasetLoader as dl
 import DatasetFormatter as df
 import CGANKerasModel as km
 import LayerDefinition as ld
+import LetterProducer as lp
 
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -21,17 +24,17 @@ import shutil
 import time
 
 #Constants
-batch_size = 32
+batch_size = cfg.GetIntValue("CGAN", "BatchSize")
 num_channels = 1
-num_classes = 10
-image_size = 28
-latent_dim = 128
-epoch_count = 25
-refreshEachStep = 20
+num_classes = cfg.GetIntValue("CGAN", "NumberOfClasses")
+image_size = cfg.GetIntValue("CGAN", "ImageSize")
+latent_dim = cfg.GetIntValue("CGAN", "LatentDimension")
+epoch_count = cfg.GetIntValue("CGAN", "EpochCount")
+refreshEachStep = cfg.GetIntValue("CGAN", "RefreshUIEachXIteration")
+imageCountToProduce = cfg.GetIntValue("CGAN", "NumberOfFakeImagesToOutput")
 
 generator_in_channels = latent_dim + num_classes
 discriminator_in_channels = num_channels + num_classes
-print(generator_in_channels, discriminator_in_channels)
 
 def train(allDatasets, gan, epochs):
     print("Training started")
@@ -74,7 +77,11 @@ def GetDatetimeFromSeconds(seconds):
 layerDefiniton = ld.LayerDefinition(discriminator_in_channels,generator_in_channels)
 
 cond_gan = km.ConditionalGAN(
-    discriminator=layerDefiniton.GetDiscriminator(), generator=layerDefiniton.GetGenerator(), latent_dim=latent_dim, imageSize=image_size, numberOfClasses=num_classes
+    discriminator=layerDefiniton.GetDiscriminator(), 
+    generator=layerDefiniton.GetGenerator(), 
+    latent_dim=latent_dim, 
+    imageSize=image_size, 
+    numberOfClasses=num_classes
 )
 cond_gan.compile(
     d_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
@@ -94,75 +101,24 @@ tensorDatasets = bulkDatasetFormatter.ProcessData();
 train(tensorDatasets,cond_gan,epoch_count)
 trained_gen = cond_gan.generator
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# interpolate
+# Use the trained generator
 sentinel = True
 while(sentinel):
-    Question = input("Enter a new index to generate (0-" + str(num_classes)+ ")(type N to exit):")
+    Question = input(f"Enter a new index to generate (0-{num_classes - 1}))(type N to exit):")
     if Question == "N":
         sentinel = False
         break
 
     value = int(Question)
 
-    # Choose the number of intermediate images that would be generated in
-    # between the interpolation + 2 (start and last images).
-    num_interpolation = 10  # @param {type:"integer"}
+    if value >= num_classes:
+        print(f"Please write numbers within 0-{num_classes - 1}")
+        continue
+    if value < 0:
+        print(f"Please write numbers within 0-{num_classes - 1}")
+        continue
 
-    # Sample noise for the interpolation.
-    interpolation_noise = tf.random.normal(shape=(1, latent_dim))
-    interpolation_noise = tf.repeat(interpolation_noise, repeats=num_interpolation)
-    interpolation_noise = tf.reshape(interpolation_noise, (num_interpolation, latent_dim))
+    letterProducer = lp.LetterProducer(trained_gen, num_classes, latent_dim)
 
-
-    def interpolate_class(first_number, second_number):
-        # Convert the start and end labels to one-hot encoded vectors.
-        first_label = keras.utils.to_categorical([first_number], num_classes)
-        second_label = keras.utils.to_categorical([second_number], num_classes)
-        first_label = tf.cast(first_label, tf.float32)
-        second_label = tf.cast(second_label, tf.float32)
-
-        # Calculate the interpolation vector between the two labels.
-        percent_second_label = tf.linspace(0, 1, num_interpolation)[:, None]
-        percent_second_label = tf.cast(percent_second_label, tf.float32)
-        interpolation_labels = (
-            first_label * (1 - percent_second_label) + second_label * percent_second_label
-        )
-
-        # Combine the noise and the labels and run inference with the generator.
-        noise_and_labels = tf.concat([interpolation_noise, interpolation_labels], 1)
-        fake = trained_gen.predict(noise_and_labels)
-        return fake
-
-    fake_images = interpolate_class(value, value)
-    fake_images *= 255.0
-
-    #Generate Gif
-    imageio.mimsave('out.gif', fake_images)
+    images = letterProducer.GenerateLetter(value, 10)
+    letterProducer.SaveImagesAsGif(images)
