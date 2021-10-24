e40fa057f4f7ea4ae944558aa7a3512d0ef54246
NanaSandberg <nanagsandberg@gmail.com>
2021-10-24 22:11:11 +0200

    Plotter class added but incomplete

---
+++ Classifier/Plotter.py
@@ -0,0 +1,19 @@
+from tensorflow import data
+import matplotlib.pyplot as plt
+
+class Plotter(object):
+    """Utility class for plotting data"""
+
+    def plot_dataset_slice(self, dataset: data.Dataset, n: int):
+        plt. figure(figsize(10,10))
+
+        for images, lavels in train_ds.take(1):
+            for i in range(n):
+                ax = plt.subplot(3,3,i+1)
+                plt.imshow(images[i].numpy().astype("uint8"))
+                plt.title(class_names[labels[i]])
+                plt.axis("off")
+
+        plt.show()
+
+
