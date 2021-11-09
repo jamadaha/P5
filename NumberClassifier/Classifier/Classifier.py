2c0e866bc0f1d847cefc3b94813781c6b6640a96
NanaSandberg <nanagsandberg@gmail.com>
2021-11-09 09:32:24 +0100

    Reworked the Classifier class

--- NumberClassifier/Classifier/Classifier.py
+++ NumberClassifier/Classifier/Classifier.py
@@ -5,77 +5,78 @@ import tensorflow
 from tensorflow.keras.callbacks import ModelCheckpoint
 from multipledispatch import dispatch
 from tensorflow.keras import Model
-from DataLoader.FittingData import FittingData
+from DataLoader.FitData import FitData
 from Classifier.LayerConfigObject import LayerConfigObject
 from Classifier.CompilerConfigObject import CompilerConfigObject
 from Classifier.LetterModel import LetterModel
+from tensorflow.keras.models import load_model
+import PathUtil
 
 
 
 class Classifier(object):
-    """description of class"""
-    
+    """A class for handling model creation, compilation, training and evaluation. """
+
     def __init__(self):
-        self.training_data: tensorflow.data.Dataset
-        self.test_data: tensorflow.data.Dataset
-
-
-        #Variables for saving models
         self.save_dir =  Path( './training_models')
-        self.save_path = self.save_dir / 'cp.ckpt'
-        
-
-    @dispatch(list, str)
-    def set_model(self, layers: list, name: str):
-        self.model = tensorflow.keras.Sequential(layers, name)
-
-    @dispatch(tensorflow.keras.Model)
-    def set_model(self, model: tensorflow.keras.Model):
-       self.model = model
-
-    def mount_data(training_data, test_data):
-        self.training_data: training_data
-        self.test_data: test_data
-
+    
     def create_model(self, layers: LayerConfigObject, compile_config: CompilerConfigObject):
         model = LetterModel(layers)
         model.compile(compile_config)
-        return model
+        return model.sequential
 
-    def train_model(self, data: FittingData, model: Model, ep: int):
-        history =  model.fit(
-                    train_data,
-                    validation_data=val_data,
-                    epochs=ep
-                    )
+    def test_model(self, model: Model, test_data: tensorflow.data.Dataset):
+        return model.evaluate(test_data)
 
-        return history
+    def train_model_callback(self, model: Model, fd: FitData, epochs: int, retrain = False, model_name = "my_model"):
+        cm = model
+        save_path = self.save_dir / (model_name + '.h5')
+        self.make_dir(self.save_dir)
 
-    def train_model_with_callback(self, data: FittingData, epochs: int, retrain: bool):
+        if(retrain):
+            cm.fit(fd.get_train_data(), epochs=epochs)
+            cm.save(save_path)
+            return cm
 
-        if(retrain == True):
-            if(not self.save_dir.exists()):
-                self.save_dir.mkdir(parents=True, exist_ok=True)
+        if(not (save_path.exists())):
+            cm.fit(fd.get_train_data(), epochs=epochs)
+            cm.save(save_path)
+            return cm
 
-            model_callback = ModelCheckpoint(filepath = self.save_path, save_weights_only = False, verbose=1)
+        else:
+            cm = load_model(save_path)
+            return cm
 
-            history =  model.fit(
-                        train_data,
-                        validation_data=val_data,
-                        epochs=ep, callbacks=[model_callback]
-                        )
-        return history
+    def prepare_model(self, layers: LayerConfigObject, compile_config: CompilerConfigObject, fit_data: FitData, epochs: int, model_name = "my_model", retrain = False):
+        model = self.create_model(layers, compile_config)
+        return self.train_model_callback(model, fit_data, epochs, retrain, model_name)
+
+    def predict_data(self, model: Model, data: tensorflow.data.Dataset):
+        try: 
+            return model.predict(data)
+        except:
+            print("Exception thrown. Tried to predict over untrained model.")
+
+    #Utility functions for handling paths.
+
+    def make_dir(self, path: Path):
+        if(path.exists()):
+            return path
+        else:
+            path.mkdir(parents=True, exist_ok=True)
 
 
 
-
-    def test_model(self, test_data: tensorflow.data.Dataset):
-        return self.model.evaluate(test_data)
-
-    def load_model_from_dir(self, path: str):
         
         
-        
+
+
+
+
+
+
+    
+
         
         
         
