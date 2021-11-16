c4dd0ec43071a3d03217790398408a6cbafa47f4
NanaSandberg <nanagsandberg@gmail.com>
2021-11-16 11:33:37 +0100

    Removed some outcommented code

--- JANGAN/Classifier/LayerConfigObject.py
+++ JANGAN/Classifier/LayerConfigObject.py
@@ -7,7 +7,6 @@ class LayerConfigObject(object):
 
     def __init__(self):
         self.layers = [
-                    #layers.Rescaling(1./255),
                     layers.Conv2D(32, 3, activation='relu'),
                     layers.MaxPooling2D(),
                     layers.Conv2D(32, 3, activation='relu'),
