e528eeeda5334d75232e9f6fe189e255e051ff0f
jamadaha <53914190+jamadaha@users.noreply.github.com>
2021-11-30 12:25:05 +0100

    Merges all images to a single

--- JANGAN/CGAN/CGANTrainer.py
+++ JANGAN/CGAN/CGANTrainer.py
@@ -47,6 +47,7 @@ class CGANTrainer():
             'GLoss': tf.summary.create_file_writer(os.path.join(logPath, 'Loss', 'GLoss')),
             'DLoss': tf.summary.create_file_writer(os.path.join(logPath, 'Loss', 'DLoss')),
             'DiffLoss': tf.summary.create_file_writer(os.path.join(logPath, 'Loss', 'DiffLoss')),
+            'Images': tf.summary.create_file_writer(os.path.join(logPath, 'Images')),
             #'Accuracy': tf.summary.create_file_writer(os.path.join(logPath, 'Accuracy'))
         }
         self.OutputDir = outputDir
@@ -188,6 +189,40 @@ class CGANTrainer():
 
         imageArray = numpy.reshape(imageArray, (len(imageArray), 28, 28, 1))
 
-        with tf.summary.create_file_writer(path + 'Log/').as_default():
-            tf.summary.image("Epoch images", imageArray, max_outputs=len(imageArray), step=epoch)
-           
+        figure = self.FigGrid(imageArray)
+
+        with self.SummaryWriter['Images'].as_default():
+            tf.summary.image("Epoch images", self.PlotToImage(figure), max_outputs=len(imageArray), step=epoch)
+    
+    def FigGrid(self, images):
+        import matplotlib.pyplot as plt
+        import math
+        figure = plt.figure(figsize=(10, 10))
+        gridSize = math.ceil(math.sqrt(len(images)))
+        for i in range(len(images)):
+            plt.subplot(gridSize, gridSize, i + 1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+            plt.tight_layout()
+            plt.imshow(images[i], cmap=plt.cm.binary)
+        return figure
+
+    #https://www.tensorflow.org/tensorboard/image_summaries
+    def PlotToImage(self, figure):
+        import io
+        import matplotlib.pyplot as plt
+        """Converts the matplotlib plot specified by 'figure' to a PNG image and
+        returns it. The supplied figure is closed and inaccessible after this call."""
+        # Save the plot to a PNG in memory.
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        # Closing the figure prevents it from being displayed directly inside
+        # the notebook.
+        plt.close(figure)
+        buf.seek(0)
+        # Convert PNG buffer to TF image
+        image = tf.image.decode_png(buf.getvalue(), channels=4)
+        # Add the batch dimension
+        image = tf.expand_dims(image, 0)
+        return image
