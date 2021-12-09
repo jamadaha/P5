d18f3ba69d9c15e06b3998d0da93682041efde84
Kristian Skov Johansen <kris701kj@gmail.com>
2021-12-09 09:42:43 +0100

    Fixed tests

--- JANGAN/ProjectTools/ConfigHelperTests.py
+++ JANGAN/ProjectTools/ConfigHelperTests.py
@@ -15,18 +15,6 @@ class ConfigHelperTests(unittest.TestCase):
         # ASSERT
         self.assertLess(0, length)
 
-    def test_CanLoadOverrideConfig(self):
-        # ARRANGE
-        from ProjectTools import ConfigHelper
-        cfg = ConfigHelper.ConfigHelper("ProjectTools/TestFiles/test-config.ini", "ProjectTools/TestFiles/override-test-config.ini")
-
-        # ACT
-        cfg.LoadConfig()
-        length : int = cfg.CategoryKeyCount("OVERRIDECATEGORY")
-
-        # ASSERT
-        self.assertLess(0, length)
-
     def test_ThrowsIfConfigFileNotFound(self):
         # ARRANGE
         from ProjectTools import ConfigHelper
@@ -36,45 +24,6 @@ class ConfigHelperTests(unittest.TestCase):
         with self.assertRaises(ConfigHelper.ConfigFileNotFoundException):
             cfg.LoadConfig()
 
-    #endregion
-
-    # Key overriding
-    #region Key Overriding
-    def test_CanOverrideKeys_String(self):
-        # ARRANGE
-        from ProjectTools import ConfigHelper
-        cfg = ConfigHelper.ConfigHelper("ProjectTools/TestFiles/test-config.ini", "ProjectTools/TestFiles/override-test-config.ini")
-        cfg.LoadConfig()
-
-        # ACT
-        value = cfg.GetStringValue("TESTCATEGORY1", "StringKey1")
-
-        # ASSERT
-        self.assertEqual("new-abc", value)
-
-    def test_CanOverrideKeys_Int(self):
-        # ARRANGE
-        from ProjectTools import ConfigHelper
-        cfg = ConfigHelper.ConfigHelper("ProjectTools/TestFiles/test-config.ini", "ProjectTools/TestFiles/override-test-config.ini")
-        cfg.LoadConfig()
-
-        # ACT
-        value = cfg.GetIntValue("TESTCATEGORY1", "IntKey1")
-
-        # ASSERT
-        self.assertEqual(111, value)
-
-    def test_CanOverrideKeys_JSON(self):
-        # ARRANGE
-        from ProjectTools import ConfigHelper
-        cfg = ConfigHelper.ConfigHelper("ProjectTools/TestFiles/test-config.ini", "ProjectTools/TestFiles/override-test-config.ini")
-        cfg.LoadConfig()
-
-        # ACT
-        value = cfg.GetJsonValue("TESTCATEGORY1", "JsonKey1")
-
-        # ASSERT
-        self.assertEqual({ "Value1": "new-1", "Value2": "new-2" }, value)
     #endregion
 
     # Key/Category checking
