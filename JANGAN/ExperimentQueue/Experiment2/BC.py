13a73a61dc45556cb72b82028547ac3fba517b3c
jamadaha <53914190+jamadaha@users.noreply.github.com>
2021-12-10 14:12:47 +0100

    Adds layers to the BC

--- JANGAN/ExperimentQueue/Experiment2/BC.py
+++ JANGAN/ExperimentQueue/Experiment2/BC.py
@@ -16,16 +16,22 @@ class LayerDefinition(Classifier.LayerDefinition.LayerDefinition):
             keras.layers.InputLayer((self.ImageSize, self.ImageSize, self.ImageChannels))
         )
         model.add(
-            keras.layers.Conv2D(128, kernel_size=(3, 3), strides=(2, 2), padding="same", kernel_initializer=init)
+            keras.layers.Conv2D(128, kernel_size=3, strides=(2, 2), padding="same", kernel_initializer=init)
         )
         model.add(
             keras.layers.LeakyReLU()
         )
         model.add(
-            keras.layers.Conv2D(128, kernel_size=(3, 2), strides=(2, 2), padding="same", kernel_initializer=init)
+            keras.layers.AveragePooling2D()
+        )
+        model.add(
+            keras.layers.Conv2D(128, kernel_size=3, strides=(2, 2), padding="same", kernel_initializer=init)
         )
         model.add(
             keras.layers.LeakyReLU()
+        )
+        model.add(
+            keras.layers.AveragePooling2D()
         )
         model.add(
             keras.layers.Flatten()
